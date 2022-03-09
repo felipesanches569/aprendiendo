a0d43442abc1c8562544bef00e0f0f0d1690bca1
felipesanches569 <felipesanches_@hotmail.com>
2022-03-09 17:10:37 -0500

    agregar minuto 30

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
+function holamundo(nombre, edad)
+{ // inicio para usar el div
+  var presentecion = 
+  (
+    <div>   
+      < h2>Hola, soy {nombre}</h2>
+      <h3>Tengo {edad} años</h3>
+    </div>
+  )
+  return presentecion;
+}
 
 function App() {
+  var nombre = "JUAN ROLDAN";
+ // var presentecion = <h2>Hola, soy {nombre}</h2>
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,12 @@ function App() {
         <p>
          HOLA MUNDO SOY JHRR...
         </p>
+        {
+          holamundo(nombre)
+        }
+        {       
+        /*  alert("te estoy robando tus datos!! llamame 3125452344")*/
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
